a97f72496f6e92dbf0e34671d5fbbf5bdf1ffa97
Daniel Tamayo <dttrujillo00@gmail.com>
2022-11-15 10:44:20 -0500

    Probando funcion para verifcar programas

--- ModularDemo_Setup/ModularDemo Setup/Program.cs
+++ ModularDemo_Setup/ModularDemo Setup/Program.cs
@@ -21,7 +21,8 @@ namespace ModularDemo_Setup
                                   new File(@"Files\Modules\ModularDemo.Core.dll"),
                                   new File(@"Files\Modules\ModularDemo.Core.pdb"),
                                   new Files(module1, @"Modulo1\*.*"),
-                                  new Files(module2, @"Modulo2\*.*"))));
+                                  new Files(module2, @"Modulo2\*.*"),
+                                   new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]"))));
 
             project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
 
@@ -45,7 +46,56 @@ namespace ModularDemo_Setup
             //project.SourceBaseDir = "<input dir path>";
             //project.OutDir = "<output dir path>";
 
+            project.UILoaded += project_UIInit;
+           // project.BeforeInstall += project_BeforeInstall;
+
+
             project.BuildMsi();
+        }
+
+       // static void project_BeforeInstall(SetupEventArgs e)
+      //  {
+       //     MessageBox.Show(e.Session.GetMainWindow(), e.ToString(), "BeforeInstall");
+       // }
+
+        static void project_UIInit(SetupEventArgs e)
+        {
+            MessageBox.Show(e.Session.GetMainWindow(), "Hello World! (CLR: v" + Environment.Version + ")", "Managed Setup - UIInit");
+
+            //set custom installdir
+            //This event is fired before Wix# ManagedUI loaded (disabled for demo purposes)
+            //e.Session["INSTALLDIR"] = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\7-Zip")
+            //                                              .GetValue("Path")
+            //                                              .ToString();
+
+  
+        }
+
+        string rutaEnElRegistro = "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+
+        private string BuscarVersionInstalada(string displayName, string publisher)
+        {
+            Microsoft.Win32.RegistryKey clave = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(rutaEnElRegistro);
+            foreach (var skname in clave.GetSubKeyNames())
+            {
+                Microsoft.Win32.RegistryKey productKey = key.OpenSubKey(skname);
+                if (productKey != null)
+                {
+                    string keyValue = Convert.ToString(productKey.GetValue("Publisher"));
+                    if (!keyValue.Equals(publisher, StringComparison.OrdinalIgnoreCase))
+                        continue;
+
+                    string productName = Convert.ToString(productKey.GetValue("DisplayName"));
+                    if (!productName.Equals(displayName, StringComparison.OrdinalIgnoreCase))
+                        continue;
+
+                    string displayVersion = Convert.ToString(productKey.GetValue("DisplayVersion"));
+
+                    return displayVersion;
+                }
+            }
+
+            return null;
         }
     }
 }
